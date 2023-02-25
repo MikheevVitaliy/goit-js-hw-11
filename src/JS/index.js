d1778a29338f30bf439561d77a06baa5001af0aa
MikheevVitaliy <mikheev.vitaliy87@gmail.com>
2023-02-25 13:22:16 +0200

    completed for verification

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,27 +1,29 @@
-import Notiflix from 'notiflix';
 import PixadayApiData from './pixadayApi.js';
 import LoadMoreBtn from './components/LoadMoreBtn.js';
 
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   formEl: document.getElementById('search-form'),
   galleryEl: document.querySelector('.gallery'),
-  searchBtnEl: document.querySelector('.load-more'),
+  // searchBtnEl: document.querySelector('.load-more'),
 };
 // ------------------------------
 const pixadayApiData = new PixadayApiData();
 
 const loadMoreBtn = new LoadMoreBtn({
   selector: '.load-more',
+  isHidden: true,
 });
 
 console.log(pixadayApiData);
 console.log(loadMoreBtn);
 // ------------------------------
 refs.formEl.addEventListener('submit', onSubmit);
-refs.searchBtnEl.addEventListener('click', loadMore);
+// refs.searchBtnEl.button.addEventListener('click', loadMore);
+loadMoreBtn.button.addEventListener('click', loadMore);
 // ------------------------------
 function onSubmit(event) {
   event.preventDefault();
@@ -33,6 +35,8 @@ function onSubmit(event) {
   clearNewsList();
 
   pixadayApiData.resetPage();
+
+  loadMoreBtn.show();
 
   loadMore()
     // ===========Одинаковые функции, убираю повторения вызовом функции
@@ -55,23 +59,92 @@ function onSubmit(event) {
     .finally(() => form.reset());
 }
 // ------------------------------
-function loadMore() {
-  return pixadayApiData
-    .fetchData(pixadayApiData.searchQuery)
-    .then(hits => {
-      if (hits.length === 0)
-        throw new Error(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      return hits.reduce(
-        (markup, article) => createMarkup(article) + markup,
-        ''
-      );
-    })
-    .then(markup => {
-      appendNewsToList(markup);
-    })
-    .catch(onError);
+// function loadMore() {
+//   loadMoreBtn.disable();
+
+//   return pixadayApiData
+//     .fetchData(pixadayApiData.searchQuery)
+//     .then(hits => {
+//       if (hits.length === 0)
+//         throw new Error(
+//           'Sorry, there are no images matching your search query. Please try again.'
+//         );
+//       return hits.reduce(
+//         (markup, article) => createMarkup(article) + markup,
+//         ''
+//       );
+//     })
+//     .then(markup => {
+//       appendNewsToList(markup);
+//       loadMoreBtn.enable();
+//     })
+//     .catch(onError);
+// }
+//---------------------------------------
+async function loadMore() {
+  loadMoreBtn.disable();
+
+  try {
+    const data = await pixadayApiData.fetchData(pixadayApiData.searchQuery);
+
+    const { hits, totalHits } = data;
+
+    // console.log(hits);
+    if (hits.length === 0) throw new Error(onNothingFound());
+    const markup = hits.reduce(
+      (markup, article) => createMarkup(article) + markup,
+      ''
+    );
+    appendNewsToList(markup);
+    loadMoreBtn.enable();
+    //----------------------
+    let page = pixadayApiData.page - 1;
+    let limitPerPage = 40;
+    // console.log(data);
+    if (pixadayApiData.page - 1 === 1) onInfo(totalHits);
+    const totalPages = totalHits / limitPerPage;
+
+    if (page > totalPages) throw new Error(onNoMore());
+  } catch (err) {
+    // console.error(err);
+    return err;
+  }
+}
+//---------------Помилки
+function onNothingFound(err) {
+  console.error(err);
+  loadMoreBtn.hide();
+  // appendNewsToList(
+  //   '<p>Sorry, there are no images matching your search query. Please try again.</p>'
+  // );
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+}
+
+function onNoMore() {
+  loadMoreBtn.hide();
+  Notiflix.Notify.info(
+    "We're sorry, but you've reached the end of search results."
+  );
+}
+
+function onInfo(info) {
+  Notiflix.Notify.success(`Hooray! We found ${info} images.`);
+}
+//-------------------------------
+function clearNewsList(markup) {
+  refs.galleryEl.innerHTML = '';
+}
+
+function appendNewsToList(markup) {
+  refs.galleryEl.insertAdjacentHTML('beforeend', markup);
+
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    enableKeyboard: true,
+  }).refresh();
 }
 // ------------------------------
 function createMarkup({
@@ -84,35 +157,27 @@ function createMarkup({
   downloads,
 }) {
   return `<div class="photo-card">
-  <img src=${webformatURL} alt=${tags} loading=${largeImageURL} />
-  <div class="info">
+    <div class="images">
+    <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy"  /></a>
+    </div>
+    <div class="info">
     <p class="info-item">
-      <b>Likes: ${likes}</b>
+      <b><span class="span">Likes:</span></b>
+      <b>${likes}</b>
     </p>
     <p class="info-item">
-      <b>Views: ${views}</b>
+      <b><span class="span">Views:</span></b>
+      <b>${views}</b>
     </p>
     <p class="info-item">
-      <b>Comments: ${comments}</b>
+      <b><span class="span">Comments:</span></b>
+      <b>${comments}</b>
     </p>
     <p class="info-item">
-      <b>Downloads: ${downloads}</b>
+      <b><span class="span">Downloads:</span></b>
+      <b>${downloads}</b>
     </p>
   </div>
 </div>`;
 }
 // ------------------------------
-function appendNewsToList(markup) {
-  refs.galleryEl.insertAdjacentHTML('beforeend', markup);
-}
-// ------------------------------
-function clearNewsList(markup) {
-  refs.galleryEl.innerHTML = '';
-}
-
-function onError(err) {
-  console.error(err);
-  updateNewsList(
-    '<p>Sorry, there are no images matching your search query. Please try again.</p>'
-  );
-}
--- src/JS/pixadayApi.js
+++ src/JS/pixadayApi.js
@@ -1,7 +1,9 @@
+import axios from 'axios';
+
 const ENDPOINT = 'https://pixabay.com/api/';
 const API_KEY = '33801420-150697ebc6b3d77194fe6b38c';
 const OPTIONS = 'image_type=photo&orientation=horizontal&safesearch=true';
-const PERPAGE = '3';
+const PERPAGE = '40';
 // const page = '2';
 
 // function fetchData(query) {
@@ -13,19 +15,35 @@ const PERPAGE = '3';
 
 export default class PixadayApiData {
   constructor() {
-    this.page = 2;
+    this.page = 1;
     this.searchQuery = '';
   }
 
-  fetchData() {
+  // fetchData() {
+  //   const URL = `${ENDPOINT}?key=${API_KEY}&${OPTIONS}&per_page=${PERPAGE}&q=${this.searchQuery}&page=${this.page}`;
+
+  //   return fetch(URL)
+  //     .then(response => response.json())
+  //     .then(({ hits }) => {
+  //       this.nextPage();
+  //       return hits;
+  //     });
+  // }
+
+  async fetchData() {
     const URL = `${ENDPOINT}?key=${API_KEY}&${OPTIONS}&per_page=${PERPAGE}&q=${this.searchQuery}&page=${this.page}`;
 
-    return fetch(URL)
-      .then(response => response.json())
-      .then(({ hits }) => {
-        this.nextPage();
-        return hits;
-      });
+    const response = await axios.get(URL);
+    this.nextPage();
+    // console.log(response.data.totalHits);
+    // return response.data.hits;
+    return response.data;
+
+    // return axios.get(URL).then(({ data }) => {
+    //   this.nextPage();
+    //   // console.log(data.hits);
+    //   return data.hits;
+    // });
   }
 
   nextPage() {
